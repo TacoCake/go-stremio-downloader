09b9f2687458452e43929654f40ba7dc64f08b1b
Tacocake <kiwifruit@seibmoz.anonaddy.me>
2025-04-26 09:01:28 -0400

    feat: add some helpers to create jellyfin compatible NFOs

---
+++ pkg/tacocake/jellyfin/Nfo.go
@@ -0,0 +1,68 @@
+package jellyfin
+
+import (
+	"encoding/xml"
+	"os"
+)
+
+type Episode struct {
+	Season  int    `xml:"season"`
+	Episode int    `xml:"episode"`
+	Title   string `xml:"title,omitempty"`
+	Plot    string `xml:"plot,omitempty"`
+}
+
+type Season struct {
+	SeasonNumber int    `xml:"seasonNumber"`
+	Title        string `xml:"title,omitempty"`
+}
+
+type Series struct {
+	Title string `xml:"title"`
+	Plot  string `xml:"plot"`
+}
+
+func (s *Series) MakeNfo(nfoPath string) error {
+	tmp := struct {
+		Series
+		XMLName struct{} `xml:"tvshow"`
+	}{Series: *s}
+
+	xmlData, err := xml.MarshalIndent(tmp, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(nfoPath, xmlData, 0775)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Season) MakeNfo(nfoPath string) error {
+	tmp := struct {
+		Season
+		XMLName struct{} `xml:"season"`
+	}{Season: *s}
+
+	xmlData, err := xml.MarshalIndent(tmp, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(nfoPath, xmlData, 0775)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (e *Episode) MakeNfo(nfoPath string) error {
+	xmlData, err := xml.MarshalIndent(e, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(nfoPath, xmlData, 0775)
+	if err != nil {
+		return err
+	}
+	return nil
+}
